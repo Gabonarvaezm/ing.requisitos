b499b4163b25a34bf222c669ab184b88d28b1dee
Gabonarvaezm <gabriel.narvaezm@campusucc.edu.co>
2024-08-13 00:12:21 -0500

    cambio en el codigo

--- punto3.py
+++ punto3.py
@@ -1,6 +1,6 @@
 print ("sueldo semanal")
-horas_trabajo = input("digite horas de trabajo: ")
-pago_horas = input("digite pago por horas de trabajo: ")
+horas_trabajo = input("escriba horas de trabajo: ")
+pago_horas = input("escriba pago por horas de trabajo: ")
 conversion = int(horas_trabajo)*int(pago_horas)
 
-print (f'el trabajador gana {conversion}')
+print (f' se gana alrededor de  {conversion}')
